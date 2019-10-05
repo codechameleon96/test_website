fba78ced3f8b1a60f980a14f099b8b5a54b8f346
amguruprasath5 <amguruprasath5@gmail.com>
2019-10-05 12:07:35 +0530

    Finished About

---
+++ script/main.js
@@ -0,0 +1,33 @@
+// var swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   mousewheel: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//     type: 'bullets',
+//   },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//     hide: false,
+//     draggable: true,
+//     snapOnRelease: true,
+//   },
+// });
+
+
+
+
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters
+  slidesPerView: 1,
+  mousewheel: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    type: 'bullets',
+  },
+
+  
+})
